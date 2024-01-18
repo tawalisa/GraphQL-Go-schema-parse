040f05727d701b0348e953932180260907ab66bd
lijia <tawalisa@163.com>
2024-01-18 17:12:26 +0800

    done query

--- schemaParser/schameParse.go
+++ schemaParser/schameParse.go
@@ -1,8 +1,8 @@
 package schemaParser
 
 import (
-	"fmt"
 	"github.com/graphql-go/graphql"
+	"reflect"
 	"regexp"
 	"strings"
 )
@@ -28,7 +28,7 @@ type GraphQLParsingField struct {
 	params   []GraphqlParingParam
 }
 
-func Parsing(sdlContent string) (*GraphQLType, error) {
+func Parsing(sdlContent string) (*graphql.Object, error) {
 
 	namespaces := strings.Split(sdlContent, "}")
 	types := []GraphQLParsingType{}
@@ -38,18 +38,14 @@ func Parsing(sdlContent string) (*GraphQLType, error) {
 		if strings.HasPrefix(ns, "type ") {
 			parsingType := GraphQLParsingType{}
 			typep, fields := splitStringPairs(ns, "{")
-			typeDef, typeName := splitSpaceAndTabPairs(typep)
+			_, typeName := splitSpaceAndTabPairs(typep)
 			parsingType.name = typeName
 			parsingType.typeName = "type"
-			fmt.Printf("解析类型：%s\n", typeName)
-			fmt.Printf("类型定义：%s\n", typeDef)
 			fs := splitNewLineAndTabPairs(fields)
 			for _, fline := range fs {
 				fieldName, fieldType := splitLastSpacePairs(fline)
 				fieldName = strings.TrimSpace(fieldName)
 				fieldType = strings.TrimSpace(fieldType)
-				fmt.Printf("字段名称：%s\n", fieldName)
-				fmt.Printf("字段类型：%s\n", fieldType)
 				f := parsingField(fieldName, fieldType)
 				if parsingType.fields == nil {
 					parsingType.fields = make(map[string]GraphQLParsingField)
@@ -60,7 +56,98 @@ func Parsing(sdlContent string) (*GraphQLType, error) {
 		}
 	}
 	println(types)
-	return nil, nil
+	graphqlObjMap := make(map[string]*graphql.Object)
+	for _, t := range types {
+
+		graphqlObj := createGraphqlObj(t, graphqlObjMap)
+		graphqlObjMap[t.name] = graphqlObj
+
+	}
+	return graphqlObjMap["Query"], nil
+}
+
+func createGraphqlObj(t GraphQLParsingType, objMap map[string]*graphql.Object) *graphql.Object {
+
+	gfs := make(graphql.Fields)
+
+	for k, v := range t.fields {
+		gfs[k] = &graphql.Field{
+			Type: graphqlType(v.typeName, objMap),
+			Args: graphQLArgs(v.params, objMap),
+		}
+	}
+
+	ob := graphql.NewObject(graphql.ObjectConfig{
+		Name:   t.name,
+		Fields: gfs,
+	})
+
+	return ob
+}
+
+func graphQLArgs(params []GraphqlParingParam, objMap map[string]*graphql.Object) graphql.FieldConfigArgument {
+	args := make(graphql.FieldConfigArgument)
+	for _, p := range params {
+		p.typeP, _ = extractArgs(p.typeP)
+		args[p.name] = &graphql.ArgumentConfig{
+			Type: graphqlType(p.typeP, objMap),
+		}
+	}
+	return args
+}
+
+func extractArgs(s string) (string, bool) {
+	if s[len(s)-1] == '!' {
+		return s[:len(s)-1], true
+	}
+	return s, false
+
+}
+
+func graphqlType(name string, objMap map[string]*graphql.Object) graphql.Output {
+	switch name {
+	case "String":
+		return graphql.String
+	case "Int":
+		return graphql.Int
+	case "BigDecimal":
+		return graphql.Float
+	default:
+		return graphql.NewList(getTypeByName(name, objMap))
+	}
+
+}
+
+func getTypeByName(name string, objMap map[string]*graphql.Object) graphql.Type {
+	name = strings.TrimSpace(name)
+	tname, isArray := extractType(name)
+	t, exist := objMap[tname]
+	if !exist {
+		panic("not find type " + name)
+	}
+	//ptrType := reflect.TypeOf(t)
+	//fmt.Println("Pointer type:", ptrType)
+	//listType, _ := reflect.TypeOf(t).Elem().(graphql.Type)
+
+	structValue := reflect.New(reflect.TypeOf(t).Elem())
+	structValue.Elem().Set(reflect.ValueOf(t).Elem())
+
+	if isArray {
+		return graphql.NewList(structValue.Interface().(graphql.Type))
+	} else {
+		return structValue.Interface().(graphql.Type)
+	}
+	return nil
+}
+
+func extractType(input string) (string, bool) {
+	pattern := `\[(.*?)\]`
+	regex := regexp.MustCompile(pattern)
+	match := regex.FindStringSubmatch(input)
+	if len(match) > 1 {
+		return match[1], true
+	}
+	return strings.TrimSpace(input), false
 }
 
 func parsingField(s string, t string) GraphQLParsingField {
@@ -91,7 +178,7 @@ func parsingFiledParam(str string) (string, []GraphqlParingParam) {
 				gpp = append(gpp, param)
 			}
 		}
-		return name, gpp
+		return strings.TrimSpace(name), gpp
 	} else {
 		panic("field format error" + str)
 	}
@@ -103,7 +190,7 @@ func splitStringPairs(s, sep string) (string, string) {
 	if len(parts) != 2 {
 		panic("the string can not split a pairs")
 	}
-	return parts[0], parts[1]
+	return strings.TrimSpace(parts[0]), strings.TrimSpace(parts[1])
 }
 
 func splitSpaceAndTabPairs(s string) (string, string) {
@@ -113,7 +200,7 @@ func splitSpaceAndTabPairs(s string) (string, string) {
 	if len(parts) != 2 {
 		panic("the string can not split a pairs")
 	}
-	return parts[0], parts[1]
+	return strings.TrimSpace(parts[0]), strings.TrimSpace(parts[1])
 }
 
 func splitNewLineAndTabPairs(s string) []string {
@@ -130,7 +217,7 @@ func splitLastSpacePairs(s string) (string, string) {
 func splitLasLetterPairs(s string, sep string) (string, string) {
 	lastSpaceIndex := strings.LastIndex(s, sep)
 	if lastSpaceIndex != -1 {
-		return s[:lastSpaceIndex], s[lastSpaceIndex+1:]
+		return strings.TrimSpace(s[:lastSpaceIndex]), strings.TrimSpace(s[lastSpaceIndex+1:])
 	} else {
 		panic("the string can not split a pairs")
 	}
--- schemaParser/schemaParser_test.go
+++ schemaParser/schemaParser_test.go
@@ -1,11 +1,15 @@
 package schemaParser
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/graphql-go/graphql"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	fileutil "graphql-go-schema-parser/util"
+	"log"
 	"os"
+	"reflect"
 	"regexp"
 	"testing"
 )
@@ -40,4 +44,108 @@ func TestParsingFiled(t *testing.T) {
 	} else {
 		fmt.Println("No match found")
 	}
+}
+
+func TestRef(t *testing.T) {
+	//tt := reflect.StructOf([]reflect.StructField{
+	//	{
+	//		Name: "Height",
+	//		Type: reflect.TypeOf(0.0),
+	//	},
+	//	{
+	//		Name: "Name",
+	//		Type: reflect.TypeOf(""),
+	//	},
+	//})
+	//ref
+	//fmt.Println(tt.NumField())
+	//fmt.Println(tt.FieldByName("Name"))
+	//fmt.Println(tt.Field(1))
+
+	emptyStruct := reflect.StructOf([]reflect.StructField{
+		{Name: "Field1", Type: reflect.TypeOf("")},
+		{Name: "Field2", Type: reflect.TypeOf(0)},
+	})
+
+	fmt.Println(emptyStruct)
+	instance := reflect.New(emptyStruct).Elem()
+	// 设置结构体的字段值
+	value1 := "Hello"
+	value2 := 42
+
+	instance.Field(0).SetString("11")
+	instance.Field(0).Set(reflect.ValueOf(value1))
+	instance.FieldByName("Field2").Set(reflect.ValueOf(value2))
+
+	// 打印结构体的字段值
+	fmt.Println(instance.Field(0).Interface()) // 输出: Hello
+	//fmt.Println(reflect.ValueOf(instance).Field(1).Interface()) // 输出: 42
+}
+
+type testStruct struct {
+}
+
+func Test1(t *testing.T) {
+	var ptr *testStruct // 声明一个int类型的指针变量
+	ptrType := reflect.TypeOf(ptr).Elem()
+	fmt.Println("Pointer type:", ptrType)
+	//ptr := graphql.NewObject(graphql.ObjectConfig{
+	//	Name: "Company"})
+
+	// 使用reflect包的TypeOf函数获取指针变量的类型
+
+}
+
+func TestGraphql(t *testing.T) {
+	root, _ := os.Getwd()
+	println(root)
+	sdl, e := fileutil.ReadFile("resource/schema/examplev1.sdl")
+	require.NoError(t, e)
+	assert.NotEqual(t, sdl, "")
+	QueryType, _ := Parsing(string(sdl))
+	schema, _ := graphql.NewSchema(graphql.SchemaConfig{
+		Query: QueryType,
+	})
+
+	query := `
+		{
+  StockPrice(symbol: "AAOI", start_date: "2023-05-10", end_date: "2023-06-10") {
+    AdjClose
+    Close
+    Date
+    High
+    Low
+    Open
+    Volume
+  }
+  company(symbol: "AAOI") {
+    Name
+    LongName
+    Address1
+    Address2
+    City
+    Zip
+    Country
+    Phone
+    Fax
+    Website
+    CompanyOfficers {
+      MaxAge
+      Name
+      Age
+      YearBorn
+      ExercisedValue
+      UnexercisedValue
+      FiscalYear
+    }
+  }
+}
+	`
+	params := graphql.Params{Schema: schema, RequestString: query}
+	r := graphql.Do(params)
+	if len(r.Errors) > 0 {
+		log.Fatalf("failed to execute graphql operation, errors: %+v", r.Errors)
+	}
+	rJSON, _ := json.Marshal(r)
+	fmt.Printf("%s \n", rJSON) // {"data":{"hello":"world"}}
 }
